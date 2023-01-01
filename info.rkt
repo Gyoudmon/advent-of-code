b2d8817a4a59c53b3b1adc586bb6fcb6bd564ca3
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2023-01-01 14:52:55 +0800

    Mission Start

---
+++ info.rkt
@@ -0,0 +1,17 @@
+#lang info
+
+(define collection "Advent of Code")
+(define pkg-desc "代码降临节")
+
+(define deps '("digimon" "pict"))
+(define build-deps '("digimon" "scribble-lib" "racket-doc"))
+
+(define pkg-authors '("WarGrey Gyoudmon Ju"))
+(define test-omit-paths 'all)
+
+(define typesettings '(["literacy/AoC.scrbl" xelatex #:always-make]))
+
+(define native-launcher-names '(["BigBang.cpp" console ;desktop
+                                 ((config SDL2)
+                                  (include [windows "C:\\opt\\vcpkg\\installed\\x64-windows\\include"])
+                                  (libpath [windows "C:\\opt\\vcpkg\\installed\\x64-windows\\lib"]))]))
