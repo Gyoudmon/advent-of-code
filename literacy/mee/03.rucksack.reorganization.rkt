f1f82e535580cf77936428be97e31fb5442d6975
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2023-01-22 14:45:17 +0800

    abstract `make-item-dict`

--- literacy/mee/03.rucksack.reorganization.rkt
+++ literacy/mee/03.rucksack.reorganization.rkt
@@ -12,6 +12,7 @@
 @handbook-chunk[<rucksack-reorganization:*>
                 (module tamer racket/base
                   <定义物品类型优先级函数>
+                  <定义物品类型字典构造函数>
                   <定义函数find-misplaced-item>)]
 
 有一只精灵的重要任务是将丛林探险物资装进背包，但不幸的是，这只精灵并没有严格照着打包指令做事，因此有部分物品需要重新整理。
@@ -94,9 +95,9 @@ vector 等； C 比较麻烦，没有现成的可用。注意，数组不能替�
                 (in-lines /dev/datin)]
 
 这段描述是不是很眼熟？正是前两天任务中需要你受累一键一键敲出来的大循环。这次，在我们深刻理解了这个大循环的前提下，
-就不必再敲那么多次键盘了。函数 @racket[in-lines] 的作用是“按行读出字符串，并将每一行依次保存在变量 @racketcommentfont{line}
-中，供循环使用”，@racket[for/list] 的名字暴露出了它(与 @racket[in-lines] 配合时)自带循环效果，并保证将每一轮循环处理得到
-的结果都搜集到列表（而不是别的什么数据结构）中去。
+就不必再敲那么多次键盘了。函数 @racket[in-lines] 的作用是“按行读出字符串，并将每一行依次保存在变量
+@racketcommentfont{line}中，供循环使用”，@racket[for/list] 的名字暴露出了它(与 @racket[in-lines] 配合时)
+自带循环效果，并保证将每一轮循环处理得到的结果都搜集到列表（而不是别的什么数据结构）中去。
 
 关于@bold{函数式编程}，现在我们只说这么多，最通俗易懂的部分。深呼吸，放松一下，做点简单的事开心一下：完成函数
 @racket[find-misplaced-item] 的本体。
@@ -106,7 +107,7 @@ vector 等； C 比较麻烦，没有现成的可用。注意，数组不能替�
                 (define find-misplaced-item
                   (lambda [line]
                     (define midpos (/ (string-length line) 2))
-                    <创建物品类型字典>
+                    <构造背包隔间的物品类型字典>
                     <查找字典中存在的物品类型>))]
 
 通过前面的例子，这个函数的功能已经很明确了，就是@question{依次检查后半段的每一个字符，看它是否出现在前半段中}，或者
@@ -118,38 +119,53 @@ vector 等； C 比较麻烦，没有现成的可用。注意，数组不能替�
 而是为什么你会想到字典这个东西？字典代表的是一种什么样的东西呢？像字典这样的@bold{能够根据关键字快速定位其内容}的数据
 结构称为@bold{哈希表}（Hash Table），简称哈希（@racket[hash]），它也是一种相当基础也相当重要的数据类型。将列表作为基
 础数据类型的语言一般也会直接提供一种或几种哈希类型，不过它们的名字就可能五花八门了。譬如，Python 的哈希就叫字典（dict）；
-C++ 的哈希叫映射（map）；C 不直接提供。
+C++ 的哈希叫映射（map）；C 不直接提供哈希表。
 
-总之，计算机也有查字典的权利和能力。于是，@question{找到放错了的物品类型}问题就转化成了@racketcommentfont{造字典-查字典}
-问题，这个字典里只包含某个背包的一个隔间里的全部物品类型。
+总之，计算机也有查字典的权利和能力。于是，@question{找到放错了的物品类型}问题就转化成了@racketcommentfont{构造字典-查字典}
+问题。这个字典里只包含某个背包的一个隔间里的全部物品类型。
 
-@handbook-chunk[<创建物品类型字典>
-                (define dict (make-hasheq))
-                
-                (for ([ch (in-string line 0 midpos)])
-                  (define n (hash-ref dict ch 0))
-                  (hash-set! dict ch (add1 n)))]
+@handbook-chunk[<构造背包隔间的物品类型字典>
+                (define dict (make-item-dict line midpos))]
 
-这个 @racket[for] 和 @racket[in-string] 组合似乎有点眼熟。没错，算法中的循环又被它俩隐藏了。函数 @racket[in-string]
-负责依次提取字符串指定范围内的字符，并分别保存在变量 @racketcommentfont{ch} 中供循环使用。每次往字典里添加条目必须要提供
-两个参数，一个代表关键字（key），一个代表该关键字在字典里的内容（value）。比如，英汉字典里的每一个词条，英文单词是其关键字，
-其对应的汉语翻译是其在字典里的内容。在本任务中，我们关注某个物品是否存在，那该物品的类型即是关键字；而其在字典中的内容是关于其
-“存在与否”的信息。考虑到同一个背包隔间里可能存在多个相同类型的物品，因此字典里与关键字关联的内容可设定为该关键字出现的次数。
-而造字典的过程，显然也涉及到查字典的步骤。
+直觉上看，构造字典似乎也是一个常用操作，值得单独定义一个函数 @racket[make-item-dict] 来做这件事。
+
+@handbook-chunk[<定义物品类型字典构造函数>
+                (code:comment #,($argv [items "物品清字符串"] [end "物品清字符串循环结束的位置"]))
+                (define (make-item-dict items end)
+                  (for/hash ([ch (in-string items 0 end)])
+                    <返回物品类型字典中的条目>))]
+
+这个 @racket[for/hash] 和 @racket[in-string] 组合似乎有点眼熟。没错，算法中的循环又被它俩隐藏了。函数 @racket[in-string]
+负责依次提取字符串指定范围内的字符，并分别保存在变量 @racketcommentfont{ch} 中供循环使用。@racket[for/hash] 则负责把每次
+循环的结果都搜集进一个哈希表里。
+
+每次往字典里添加条目必须要提供两个值，一个代表关键字（key），一个代表该关键字在字典里的内容（value）。譬如，
+英汉字典里的每一个词条，英文单词是其关键字，其对应的汉语翻译是其在字典里的内容。在本任务中，我们关注某个物品是否存在，
+那该物品的类型即是关键字；而其在字典中的内容是关于其“存在与否”的信息，也即布尔真值 @racket[#true]。
+
+@handbook-chunk[<返回物品类型字典中的条目>
+                (values ch #true)]
+
+注意，跟其他(绝大多数)语言不同的是，Racket 函数可以有@bold{多个}返回值。@racket[for/hash] 要求每轮循环结束提供两个值，
+那我们就通过 @racket[values] 函数一次返回两个值。
+
+Python 可以通过@bold{自动序列装包/拆包}达到同样的目的，但那仍然只是返回一个值。其具体过程是：将多个返回值搜集进一个元组，
+然后将这个元组作为单个值返回，接着再一个一个把这个元组里的值取出来分别赋值给其他变量。
 
 @handbook-chunk[<查找字典中存在的物品类型>
                 (for/first ([ch (in-string line midpos)]
                             #:when (hash-has-key? dict ch))
                   ch)]
 
-这次隐藏循环的是 @racket[for/first]，它的意思@racketcommentfont{循环执行一次就立即退出}，因此确定循环是否执行的条件至关重要。
-在本任务中，即：一旦出现字典中存在的物品类型就不用再继续找下去了，直接返回该物品类型即可。特别的，如果输入文件有错误导致循环一次
-都没有执行，那函数最终会返回 @racket[#false]。在真实的软件中，必须要考虑这种情况。
+这次隐藏循环的是 @racket[for/first]，它的意思@racketcommentfont{收集到循环执行一次的结果就立即退出}，因此确定循环是否执行
+的条件至关重要。在本任务中，即：一旦出现字典中存在的物品类型就不用再继续找下去了，直接返回该物品类型即可。
+
+特别的，如果输入参数不合理导致循环一次都没有执行，那函数最终会返回 @racket[#false]。在真实的软件中，必须要考虑这种情况。
 
 至此，我们完成了对物品清单的解析。至于为什么要同时定义 @racket[find-misplaced-items] 和 @racket[find-misplaced-item]
 两个函数，后面这个看似完全多余啊，算法可以直接放在 @racket[find-misplaced-items] 循环里面吗？可以啊，不影响本次任务。
 不过，上面虽然定义了（复数形式的） @racket[find-misplaced-items]，但它仅仅只是出现在本文档中用作演示常用循环，其代码并没有
-实际包含在最终的可执行程序里。因为这个循环过于常用，以至于我们完全没必要每次都敲一遍，把精力省下来关注@question{如何将每一行
+实际包含在最终的可执行程序里。因为这个循环过于常用，我们完全没必要每次都敲一遍，把精力省下来关注@question{如何将每一行
 杂乱无章的原始文本转化为对我们有用的数据和信息}才是正经。
 
 而且，还记得我们上面分析的结论吗：@racketcommentfont{针对清单中的每一行的操作}就转化为了@racketcommentfont{针对字符串列表中
@@ -172,7 +188,7 @@ C++ 的哈希叫映射（map）；C 不直接提供。
 函数的输出跟例子不能说完全相同，简直就是一摸一样啊。
 
 不过呢，本算法仍然还有改进空间，一丝不苟地为一个隔间创建完整的字典总觉得很没必要（比如，当放错了的物品恰好在两个隔间里都是第一个时），
-可不可以一边创建一边检查呢？请课后思考并写程序验证你想法。【提示：同时给两个隔间分别创建一个字典，检查时两个隔间同时交叉比较。】
+可不可以一边创建一边检查呢？请课后思考并尽量写程序验证你想法。【提示：同时给两个隔间分别创建一个字典，检查时两个隔间同时交叉比较。】
 
 以上，是我们为应对精灵的信口开河而做的准备工作。接下来就该处理精灵们真正要你解决的问题了。为了定好出错物品类型的重组顺序，精灵们规定
 每种类型的物品都有一个优先级：
@@ -183,19 +199,19 @@ C++ 的哈希叫映射（map）；C 不直接提供。
  ]
 
 @handbook-chunk[<定义物品类型优先级函数>
-                (code:comment #,($argv [type "代表物品类型的字符"]))
-                (define (item-type-priority type)
-                  (cond [(char<=? #\a type #\z)
-                         (+ (char->integer type) (- 97) 1)]
-                        [(char<=? #\A type #\Z)
-                         (+ (char->integer type) (- 65) 27)]
+                (code:comment #,($argv [ich "代表物品类型的字符"]))
+                (define (item-type-priority ich)
+                  (cond [(char<=? #\a ich #\z)
+                         (+ (char->integer ich) (- 97) 1)]
+                        [(char<=? #\A ich #\Z)
+                         (+ (char->integer ich) (- 65) 27)]
                         [else 0]))
 
-                (code:comment #,($argv [types "代表物品类型的字符列表"]))
-                (define (priority-sum types)
-                  (for/sum ([type (in-list types)]
-                            #:when (char? type))
-                    (item-type-priority type)))]
+                (code:comment #,($argv [items "代表物品类型的字符列表"]))
+                (define (priority-sum items)
+                  (for/sum ([ich (in-list items)]
+                            #:when (char? ich))
+                    (item-type-priority ich)))]
 
 优先级函数没啥特别要说的，如果对其中的神秘数字 @racket[97] 和 @racket[65] 感兴趣，可以@bold{谷歌}关键字 @bold{ASCII 码}，
 看看字母 @litchar{A} 和 @litchar{a} 在该编码中的具体位置。
@@ -209,10 +225,11 @@ C++ 的哈希叫映射（map）；C 不直接提供。
 
 @tamer-action[#:requires ["../aoc.rkt"]
               (code:comment "注意，函数 read-aoc-data 隐藏了读文件小循环")
-              (priority-sum
-               (read-aoc-data
-                #:from "mee/03.rr.dat"
-                #:for-each-do find-misplaced-item))]
+              (define all-misplaced-items
+                (read-aoc-data
+                 #:from "mee/03.rr.dat"
+                 #:for-each-do find-misplaced-item))
+              (priority-sum all-misplaced-items)]
 
 @;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 @handbook-reference[]
